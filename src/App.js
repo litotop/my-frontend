fe19bfa110291d7af022367e8dd17dda20a70d23
litotop <li@totop.vip>
2025-02-10 01:59:01 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>你好，React！🚀</h1>
+      <p>我的前端已经成功运行！</p>
+    </div>
+  );
+}
+
+export default App;
